e0049833d0d4891230549e07b62ec4b2db62a2df
RickyDZN <tigandenis@gmail.com>
2025-07-09 17:48:43 +0300

    front-end

--- src/main/java/org/example/controller/CompanyController.java
+++ src/main/java/org/example/controller/CompanyController.java
@@ -2,10 +2,7 @@ package org.example.controller;
 
 import org.example.service.AnafApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/company")
